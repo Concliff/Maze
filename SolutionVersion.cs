ddc42f042504682908ece4103664b3ce8ec25d49
Romman <s.roma@tut.by>
2012-04-23 00:25:23 +0200

    [0.4.2.68] Created Solution Version

---
+++ SolutionVersion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Maze
+{
+    class SolutionVersion
+    {
+        private const uint Major    = 0;
+        private const uint Minor    = 4;
+        private const uint Build    = 2;
+        private const uint Revision = 68;
+
+
+        public static string GetVersion()
+        {
+            return  Major.ToString() + "." +
+                    Minor.ToString() + "." +
+                    Build.ToString() + "." +
+                    Revision.ToString();
+
+        }
+    }
+}
