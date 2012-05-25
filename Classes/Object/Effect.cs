93274f07a5c553e4731550ea6362dc5c6656d774
Ira19 <ira190489@yandex.ru>
2012-05-25 22:34:52 +0300

    [0.5.4.130] Add DataTableReader

--- Classes/Object/Effect.cs
+++ Classes/Object/Effect.cs
@@ -58,8 +58,6 @@ namespace Maze.Classes
 
         public int Modifier;
 
-        private string BaseDirectoryPath = "Data\\Base\\";
-
         public Effect(EffectEntry effectEntry, Unit target, Object caster)
         {
             this.effectEntry = effectEntry;
@@ -70,18 +68,6 @@ namespace Maze.Classes
 
             target.ApplyEffect(this);
             effectState = EffectState.Applied;
-
-            //WorkDBF WDBF = new WorkDBF(BaseDirectoryPath);
-            //string TableName = "EFFECTSD";
-            //WDBF.CreateTable(TableName);
-            //WDBF.FillTable(TableName2, "1", "Wall", "1", "10", "10", "The player creates a barrier in front of himself from the slime through which the monster can not pass");
-            //WDBF.FillTable(TableName2, "2", "Invulnerability", "2", "6", "5", "The player becomes immune to monsters attacks for some time");
-            //WDBF.FillTable(TableName2, "3", "Accelerator", "3", "4", "10", "The player gets a bonus to the speed of movement for X seconds");
-            //DataTable table3 = WDBF.Execute("CREATE TABLE Clients (EffectId char(4), EffectName char(64), EffectType char(3), EffectValue char(10), Duration char(5), Description char(254))");
-            //DataTable table = WDBF.GetAll(TableName);
-            //DataTable table2 = WDBF.Execute("SELECT EFFECTID, EFFECTNAME FROM " + TableName);
-
-            //WDBF.FillTable(TableName, 2, "Invulnerability", 2, 3, 2, 1, 4, 10, "The player becomes immune to monsters attacks for some time");
         }
 
         public new EffectTypes GetType()
--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -10,7 +10,7 @@ namespace Maze
         private const uint Major    = 0;
         private const uint Minor    = 5;
         private const uint Build    = 4;
-        private const uint Revision = 129;
+        private const uint Revision = 130;
 
 
         public static string GetVersion()
