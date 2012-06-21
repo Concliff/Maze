138aaef333227c87ad19e5d3a0cf1e9a353bf352
Romman <s.roma@tut.by>
2012-06-21 20:45:10 +0200

    [0.5.5.138] Correct Player size

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -11,7 +11,7 @@ namespace Maze
         private const uint Major    = 0;
         private const uint Minor    = 5;
         private const uint Build    = 5;
-        private const uint Revision = 137;
+        private const uint Revision = 138;
 
 
         public static string GetVersion()
