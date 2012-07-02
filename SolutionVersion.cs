a4a5e324ff85cafc5135a73728efe19661c31bd2
Ira19 <ira190489@yandex.ru>
2012-07-03 01:59:44 +0300

    [0.5.6.170] Add images for the auras of the effects

--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -11,7 +11,7 @@ namespace Maze
         private const uint Major    = 0;
         private const uint Minor    = 5;
         private const uint Build    = 6;
-        private const uint Revision = 169;
+        private const uint Revision = 170;
 
 
         public static string GetVersion()
