aafe0b8f9b62c47b9dd7252f15066a6454ac2a9e
Ira19 <ira190489@yandex.ru>
2012-11-18 02:29:53 +0300

    [0.7.0.246] Create CollectionID class

---
+++ Custom/CollectionID.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Maze.Custom
+{
+    public class CollectionID<T> : IEnumerable
+    {
+        protected Dictionary<int, T> dictionary;
+        protected T defaultValue;
+
+        public T this[int id]
+        {
+            get
+            {
+                T val;
+                if (dictionary.TryGetValue(id, out val))
+                {
+                    return val;
+                }
+                else return defaultValue;
+            }
+            set
+            {
+                dictionary[id] = value;
+            }
+        }
+
+        public CollectionID()
+        {
+            dictionary = new Dictionary<int,T>();
+            defaultValue = default(T);
+        }
+
+        public CollectionID(T defValue) : this()
+        {
+            defaultValue = defValue;
+        }
+
+        public int GetIDs(ref int[] ids)
+        {
+            ids = dictionary.Keys.ToArray();
+            return ids.Count();
+        }
+
+        /// <summary>
+        /// Remove all keys and values from the dictionary
+        /// </summary>
+        public void Clear()
+        {
+            dictionary.Clear();
+        }
+
+        /// <summary>
+        /// Get key value and remove it from the dictionary
+        /// </summary>
+        /// <param name="index">Key in the dictionary</param>
+        /// <returns></returns>
+        public T ExtractValue(int index)
+        {
+            T val = this[index];
+            RemoveValue(index);
+            return val;
+        }
+
+        /// <summary>
+        /// Remove the key-value pair from the dictionary
+        /// </summary>
+        /// <param name="index">Key in the dictionary</param>
+        /// <returns></returns>
+        public bool RemoveValue(int index)
+        {
+            if (dictionary.Remove(index))
+                return true;
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public CollectionEnum<T> GetEnumerator()
+    {
+        return new CollectionEnum<T>(dictionary);
+    }
+    }
+
+    public class CollectionEnum<T> : IEnumerator
+    {
+        Dictionary<int, T> collection;
+
+        int position = -1;
+
+        public CollectionEnum(Dictionary<int, T> dictionary)
+        {
+            collection = dictionary;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < collection.Count);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public T Current
+        {
+            get
+            {
+                try
+                {
+                    return collection[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
--- SolutionVersion.cs
+++ SolutionVersion.cs
@@ -11,7 +11,7 @@ namespace Maze
         private const uint Major    = 0;
         private const uint Minor    = 7;
         private const uint Build    = 0;
-        private const uint Revision = 245;
+        private const uint Revision = 246;
 
 
         public static string GetVersion()
